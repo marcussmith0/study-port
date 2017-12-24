b4b5b719cb731919a2fb985c6aaacdb66c6b67b2
Marcus Smith <marcussmith0711@gmail.com>
2017-12-23 19:01:54 -0600

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({success: "you have successfully loaded the home page"});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("listening on....", PORT);
+});
---
+++ routes/authRoutes.js
@@ -0,0 +1,6 @@
+const passport = require("passport");
+
+module.exports = app => {
+    app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"]}));
+
+}
